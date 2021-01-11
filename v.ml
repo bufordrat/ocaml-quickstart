5080f7cbf45a2e13fe4cb3b70c8a8968bb695e8a
Matt Teichman <teichman@uchicago.edu>
2021-01-11 12:08:29 -0600

    initial version using dune/lib carcass

---
+++ v.ml
@@ -0,0 +1,8 @@
+(*VERSIOJ_SENTINEL*)
+(* DO NOT EDIT; this file is automagically generated. *)
+let data = Versioj.all [
+  "compiled", "2021-01-11T17:49:29Z";
+  "ident", "77af005dbad6cc857358d1e980acd267b85b84ff";
+  "vcs", "git";
+]
+
